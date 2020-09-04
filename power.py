658f63b5f60f7b093531d7b5bb454b9657a728ee
KillerTheBee <61102077+KillerTheBee@users.noreply.github.com>
2020-09-04 01:52:08 -0400

    Update power.py

--- power.py
+++ power.py
@@ -12,7 +12,7 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-b", "--bssid", dest="target", help="The BSSID of your victim.")
     parser.add_option("-i", "--interface", dest="interface", help="The name of the interface you're going to use for the attack")
-    parser.add_option("-p", "--packets", dest="packets", "The number of deauthentication packet's to send to the victim. (0 = unlimited)")
+    parser.add_option("-p", "--packets", dest="packets", help="The number of deauthentication packet's to send to the victim. (0 = unlimited)")
     (options, arguments) = parser.parse_args()
     if not options.target:
         parser.error("Please specify a target using the -t or --target parameter.\nUse --help for more informations")
@@ -22,12 +22,12 @@ def get_arguments():
         parser.error("Please specify the number of packet's you wan't to send.\nUse --help for more informations.")
     return options
 
-def attack(target, interface, packets):
+def power(target, interface, packets):
     i = 1
     while i < 4:
         print(i)
         os.system("sleep 1")
-        i++
+        i+=1
         
     print("POOOOOOOOOOWWWWWWWWEEEEEEEEEERRRRRRRRRRRRR !!!!!!!!!")
     os.system("sleep 3")
@@ -37,4 +37,4 @@ def attack(target, interface, packets):
 
 
 options = get_arguments()
-attack(options.target,options.interface,options.packets)
+power(options.target,options.interface,options.packets)
