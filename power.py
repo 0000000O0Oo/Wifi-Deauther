c39e1418a98cb0c5dc1bf553976957add67fc7ac
KillerTheBee <61102077+KillerTheBee@users.noreply.github.com>
2020-09-04 01:47:53 -0400

    Update power.py

--- power.py
+++ power.py
@@ -13,7 +13,7 @@ def get_arguments():
     parser.add_option("-b", "--bssid", dest="target", help="The BSSID of your victim.")
     parser.add_option("-i", "--interface", dest="interface", help="The name of the interface you're going to use for the attack")
     parser.add_option("-p", "--packets", dest="packets", "The number of deauthentication packet's to send to the victim. (0 = unlimited)")
-    (target, interface, packets) = parser.parse_args()
+    (options, arguments) = parser.parse_args()
     if not options.target:
         parser.error("Please specify a target using the -t or --target parameter.\nUse --help for more informations")
     elif not options.interface:
